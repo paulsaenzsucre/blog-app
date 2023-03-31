be77becc3597762d761e09dbcecc1310b65c0e47
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-03-31 01:19:19 -0500

    Implementing  Rails Active Callbacks - after_save.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
-  def self.update_comment_counter(post)
+  after_save :update_comment_counter
+
+  private
+
+  def update_comment_counter
     post.update(commentsCounter: post.comments.size)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  def self.update_post_counter(author)
-    author.update(postsCounter: author.posts.size)
-  end
-
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.update(postsCounter: author.posts.size)
   end
 end

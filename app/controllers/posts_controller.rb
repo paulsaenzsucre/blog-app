d5669c177735b24f3ac4113cbee9a46828ae8884
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-08 11:11:32 -0500

    Add new and create actions to post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,25 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.author = current_user
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to user_posts_path }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end

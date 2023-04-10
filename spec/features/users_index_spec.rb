a27098588b49a78f81587e2208161474bb968a14
diego <dgcuenca@gmail.com>
2023-04-10 17:26:53 -0500

    Add test for user display

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "UsersIndices", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'When testing user index page' do
+      scenario 'should show the username of all other users.' do
+      visit users_path
+      expect(page).to have_content(['Don G. Adams', 'Les J. Haley', 'Caroline G. Metcalf', 'Terry B. Benavidez','Rebecca J. Morris'])
+    end
+end
 end

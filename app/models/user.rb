67883d97e8195607d7f8fa55244c6035d1ede5ae
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-14 08:07:06 -0500

    Add button to delete Posts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,13 @@ class User < ApplicationRecord
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
+
+  
+  def admin?
+    self.role == "admin"
+  end
+
+  def present?
+    false 
+  end
 end
